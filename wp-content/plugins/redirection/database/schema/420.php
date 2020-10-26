4e8d80dce2a8121fbbc4816dca1fa43a4a42c443
solcra <carlosgranadacra@gmai.com>
2020-10-25 20:55:40 -0500

    Se agrega acuaizaciones al wordpress

---
+++ wp-content/plugins/redirection/database/schema/420.php
@@ -0,0 +1,51 @@
+<?php
+
+class Red_Database_420 extends Red_Database_Upgrader {
+	public function get_stages() {
+		return [
+			'add_extra_logging' => 'Add extra logging support',
+			'remove_module_id' => 'Remove module ID from logs',
+			'remove_group_id' => 'Remove group ID from logs',
+			'add_extra_404' => 'Add extra 404 logging support',
+			'allow_null_agent' => 'Allow null log agent',
+			'convert_404_url_to_text' => 'Convert 404 url to text',
+		];
+	}
+
+	protected function remove_module_id( $wpdb ) {
+		return $this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_logs` DROP `module_id`" );
+	}
+
+	protected function remove_group_id( $wpdb ) {
+		return $this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_logs` DROP `group_id`" );
+	}
+
+	protected function add_extra_logging( $wpdb ) {
+		// Update any URL with a double slash at the end
+		$this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_logs` ADD `domain` VARCHAR(255) NULL DEFAULT NULL AFTER `url`" );
+		$this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_logs` ADD `http_code` INT(11) unsigned NOT NULL DEFAULT 0 AFTER `referrer`" );
+		$this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_logs` ADD `request_method` VARCHAR(10) NULL DEFAULT NULL AFTER `http_code`" );
+		$this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_logs` ADD `redirect_by` VARCHAR(50) NULL DEFAULT NULL AFTER `request_method`" );
+
+		return $this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_logs` ADD `request_data` MEDIUMTEXT NULL DEFAULT NULL AFTER `request_method`" );
+	}
+
+	protected function allow_null_agent( $wpdb ) {
+		return $this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_logs` CHANGE COLUMN `agent` `agent` MEDIUMTEXT NULL" );
+	}
+
+	protected function add_extra_404( $wpdb ) {
+		// Update any URL with a double slash at the end
+		$this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_404` ADD `domain` VARCHAR(255) NULL DEFAULT NULL AFTER `url`" );
+		$this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_404` ADD `http_code` INT(11) unsigned NOT NULL DEFAULT 0 AFTER `referrer`" );
+		$this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_404` ADD `request_method` VARCHAR(10) NULL DEFAULT NULL AFTER `http_code`" );
+
+		return $this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_404` ADD `request_data` MEDIUMTEXT NULL DEFAULT NULL AFTER `request_method`" );
+	}
+
+	protected function convert_404_url_to_text( $wpdb ) {
+		// Same as log table
+		$this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_404` DROP INDEX `url`" );
+		return $this->do_query( $wpdb, "ALTER TABLE `{$wpdb->prefix}redirection_404` CHANGE COLUMN `url` `url` MEDIUMTEXT NOT NULL" );
+	}
+}
--- wp-content/plugins/redirection/redirection-version.php
+++ wp-content/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.8' );
-define( 'REDIRECTION_BUILD', '7acce5951ed37d0a438ffb92e849136e' );
+define( 'REDIRECTION_VERSION', '4.9' );
+define( 'REDIRECTION_BUILD', '2dd41d397208f86537f3e2656caeb502' );
 define( 'REDIRECTION_MIN_WP', '4.6' );
